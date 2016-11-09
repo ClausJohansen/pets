96214d985e695b35020d1a670ab6f4252acbf053
Admin <clausfjohansen@gmail.com>
2016-11-09 10:35:51 +0100

    haaaands!

--- Pets/Program.cs
+++ Pets/Program.cs
@@ -10,6 +10,23 @@ namespace Pets
     {
         static void Main(string[] args)
         {
+            Species dog = new Species("Dog", 2, 4);
+            Species spider = new Species("Spider", 8, 8);
+
+            Pet myPet = new Pet(dog, "Sinus");
+            Pet notMyPet = new Pet(spider, "Peter");
+
+            Person claus = new Person("Claus", 1980, myPet);
+
+            Console.WriteLine("{0} is {1}'s pet.", claus.pet.name, claus.name);
+            Console.WriteLine("{0} is a {1}.", claus.pet.name, claus.pet.species.name);
+            Console.WriteLine("{0} has {1} eyes and {2} legs.", claus.pet.name, claus.pet.species.numberOfEyes, claus.pet.species.numberOfLegs);
+
+
+            Console.WriteLine("{0} is a {1}. He \"kravler op af muren\".", notMyPet.name, notMyPet.species.name);
+            Console.WriteLine("{0} has {1} eyes and {2} legs.", notMyPet.name, notMyPet.species.numberOfEyes, notMyPet.species.numberOfLegs);
+
+            Console.ReadKey();
         }
     }
 }
